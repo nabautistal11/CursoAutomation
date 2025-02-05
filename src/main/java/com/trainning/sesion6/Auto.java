c1569e71176236ae86d815e32b209286fda8d712
Ale <nabautistal11@outlook.com>
2025-02-05 07:39:07 -0500

    Notes update and new class Main

--- src/main/java/com/trainning/sesion6/Auto.java
+++ src/main/java/com/trainning/sesion6/Auto.java
@@ -1,5 +1,15 @@
 package com.trainning.sesion6;
 
-public class Auto {
+public class Auto extends Vehiculo {
+    /*
+    Padre-----------Vehiculo
+    Hijo-------------------------Auto
+    Hijo-------------------------Barco
+    Hijo-------------------------Moto
+     */
+
+
 
 }
+
+
---
+++ src/main/java/com/trainning/sesion6/Main.java
@@ -0,0 +1,10 @@
+package com.trainning.sesion6;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto a = new Auto();
+        a.setColor("Blanco");
+
+    }
+
+}
